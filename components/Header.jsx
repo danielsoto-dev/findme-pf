182af8508b7815ea8c8cde29708e8536829f437d
Daniel Soto Jaimes <danielsoto@uninorte.edu.co>
2022-02-17 17:26:19 -0500

    Initial Setup of auth0

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import { Link } from "./Link";
+import { useUser } from "@auth0/nextjs-auth0";
 export const Header = () => {
+  const { user, error, isLoading } = useUser();
+  console.log(user);
   return (
     <header className="flex justify-around items-center h-[80px]">
       <h1 className="text-4xl">
-        <Link animated={true} href="/">
+        <Link href="/">
           Find<span className="text-blue-500">Me</span>
         </Link>
       </h1>
@@ -14,15 +17,36 @@ export const Header = () => {
         <Link href="/contact-us">Contact Us</Link>
       </nav>
       <div className="justify-around items-center flex w-[180px]">
-        <Link href="/sign-up">Sign Up</Link>
-        <Link
-          href="/log-in"
-          animated={false}
-          className="border-[1px] border-black rounded-md px-3 py-[5px]"
-        >
-          Log In
-        </Link>
+        {user && !isLoading && !error ? (
+          <LoggedIn user={user} />
+        ) : (
+          <NotLoggedIn />
+        )}
       </div>
     </header>
+  );
+};
+
+const NotLoggedIn = () => {
+  return (
+    <>
+      <a href="/sign-up">Sign Up</a>
+      <a
+        href="/api/auth/login"
+        className="border-[1px] border-black rounded-md px-3 py-[5px]"
+      >
+        Log In
+      </a>
+    </>
+  );
+};
+const LoggedIn = ({ user }) => {
+  return (
+    <>
+      <span className="text-blue-500 mr-2">{user.email}</span>
+      <span className="text-blue-500">
+        <a href="/api/auth/logout">Logout</a>
+      </span>
+    </>
   );
 };
