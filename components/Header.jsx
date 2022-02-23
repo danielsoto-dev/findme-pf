de4905db7b3b80aa4fcada51d6bb3480de795aec
Daniel Soto Jaimes <68352553+danielsoto-dev@users.noreply.github.com>
2022-02-23 16:34:22 -0500

    css: better Header Layout

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,17 +12,19 @@ export const Header = () => {
           Find<span className="text-blue-500">Me</span>
         </Link>
       </h1>
-      <nav className="justify-around items-center flex w-[180px]">
-        <Link href="/about">About Us</Link>
-        <Link href="/contact-us">Contact Us</Link>
-      </nav>
-      <div className="justify-around items-center flex min-w-[240px]">
+      <nav className="justify-around items-center flex ">
+        <Link className="mx-4" href="/about">
+          About Us
+        </Link>
+        <Link className="mx-4" href="/contact-us">
+          Contact Us
+        </Link>
         {user && !isLoading && !error ? (
           <LoggedIn user={user} />
         ) : (
           <NotLoggedIn />
         )}
-      </div>
+      </nav>
     </header>
   );
 };
@@ -30,8 +32,11 @@ export const Header = () => {
 const NotLoggedIn = () => {
   return (
     <>
-      <a href="/sign-up">Sign Up</a>
+      <a className="mx-4 underline-effect" href="/sign-up">
+        Sign Up
+      </a>
       <a
+        className="mx-4"
         href="/api/auth/login"
         className="border-[1px] border-black rounded-md px-3 py-[5px]"
       >
